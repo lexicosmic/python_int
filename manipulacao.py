d1afddc2ed983ef040c65858eeaac6ca0ff8f416
devgabmal <devgabmal@gmail.com>
2022-08-18 07:15:47 -0300

    Escreve em arquivos

--- manipulacao.py
+++ manipulacao.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 
-arquivo = open("arquivo.txt")
+# # Leitura
+# arquivo = open("arquivo.txt")
 
 # linhas = arquivo.readlines()
 
 # for linha in linhas:
 #     print(linha)
 
-texto_completo = arquivo.read()
-print(texto_completo)
+# texto_completo = arquivo.read()
+# print(texto_completo)
+# arquivo.close()
+
+# Escrita
+arquivo2 = open("arquivo2.txt", "w")
+arquivo2.write("Este é o meu outro arquivo\n")
+arquivo2.close()
+arquivo2 = open("arquivo2.txt", "a")
+arquivo2.write("Estou escrevendo uma nova linha\n")
+arquivo2.close()

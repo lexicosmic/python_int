63371fddf1985153266eb9da58ece271e67700a7
devgabmal <devgabmal@gmail.com>
2022-08-18 05:06:13 -0300

    Utiliza estruturas de repetição

--- script.py
+++ script.py
@@ -42,21 +42,47 @@
 # print(var_x == var_y or var_x == var_z)
 # print(var_x == var_y or var_x == var_z and var_z == var_y)
 
-# Estruturas condicionais
-var_x = 1
-var_y = 1000000
-if var_x > var_y:
-    print("X é maior que Y")
-elif var_y > var_x:
-    print("Y é maior que X")
-else:
-    print("X é igual a Y")
+# # Estruturas condicionais
+# var_x = 1
+# var_y = 1000000
+# if var_x > var_y:
+#     print("X é maior que Y")
+# elif var_y > var_x:
+#     print("Y é maior que X")
+# else:
+#     print("X é igual a Y")
 
+# var_x = 1
+# var_y = 1
+# if var_x > var_y:
+#     print("X é maior que Y")
+# elif var_y > var_x:
+#     print("Y é maior que X")
+# else:
+#     print("X é igual a Y")
+
+# Estruturas de repetição
 var_x = 1
-var_y = 1
-if var_x > var_y:
-    print("X é maior que Y")
-elif var_y > var_x:
-    print("Y é maior que X")
-else:
-    print("X é igual a Y")
+while var_x < 10:
+    print(var_x, end=" ")
+    var_x += 1
+
+lista1 = [1, 2, 3, 4, 5]
+lista2 = ["Olá", "Mundo", "!"]
+lista3 = [0, "Olá", "biscoito", 9.87, True]
+
+for elem in lista1:
+    print(elem, end=" ")
+print()
+
+for elem in lista2:
+    print(elem, end=" ")
+print()
+
+for elem in lista3:
+    print(elem, end=" ")
+print()
+
+for i in range(10, 20, 2):
+    print(i, end=" ")
+print()

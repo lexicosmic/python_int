56b232b71d735c0d47da1e52967a65692ce63109
devgabmal <devgabmal@gmail.com>
2022-08-18 04:43:13 -0300

    Utiliza variáveis

--- script.py
+++ script.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 
-# Imprime Olá, Mundo!
+# # Imprime Olá, Mundo!
 # print("Olá, mundo!")
 
-# Operações matemáticas
-print(2+2)
-print(2**3)
-print(10/3)
-print(10//3)
-print(10 % 3)
+# # Operações matemáticas
+# print(2+2)
+# print(2**3)
+# print(10/3)
+# print(10//3)
+# print(10 % 3)
+
+# Variáveis
+minha_variavel = "Olá, mundo!"
+print(minha_variavel)
+inteira = 1  # integer
+real = 1.1  # float
+textual = "Eu sou uma string"  # string
+booleana = True  # bool
+booleana = False  # bool
+print(inteira)
+print(real)
+print(textual)
+print(booleana)

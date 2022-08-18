d9875872c2f7c6c7bd37303647146cc5237af12e
devgabmal <devgabmal@gmail.com>
2022-08-18 13:55:00 -0300

    Utiliza função enumerate

--- listas_avancado.py
+++ listas_avancado.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 
-#  List comprehension
-lista1 = [1, 2, 3, 4, 5]
-lista2 = [i**2 for i in lista1]
-lista3 = [i for i in lista1 if i % 2 != 0]
+# #  List comprehension
+# lista1 = [1, 2, 3, 4, 5]
+# lista2 = [i**2 for i in lista1]
+# lista3 = [i for i in lista1 if i % 2 != 0]
 
-print("Usando list comprehension")
-print(lista1)
-print(lista2)
-print(lista3)
+# print("Usando list comprehension")
+# print(lista1)
+# print(lista2)
+# print(lista3)
+
+# Função enumerate
+lista1 = ["abacate", "bola", "cachorro"]
+for i, nome in enumerate(lista1):
+    print(f"{i}: {nome}")

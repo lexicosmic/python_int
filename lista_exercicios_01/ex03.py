d950c4e268bc2142df615be888d91c1dc3c1ae1b
devgabmal <devgabmal@gmail.com>
2022-08-18 08:23:13 -0300

    Corrige o exercício 3 da lista 1

--- lista_exercicios_01/ex03.py
+++ lista_exercicios_01/ex03.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 
-coef0 = float(input("Digite o coeficiente de grau 0: "))
-coef1 = float(input("Digite o coeficiente de grau 1: "))
-x1 = float(input("Digite a variável de grau 1: "))
-coef2 = float(input("Digite o coeficiente de grau 2: "))
-x2 = float(input("Digite a variável de grau 2: "))
+from cmath import sqrt
 
-resultado = coef2 * (x2 ** 2) + coef1 * x1 + coef0
+a = float(input("Digite o coeficiente A: "))
+b = float(input("Digite o coeficiente B: "))
+c = float(input("Digite o coeficiente C: "))
 
-print(f"Reultado: {resultado}")
-# 2*3^2 + 0*9 + -2 = 16
+delta = (b**2) - 4 * a * c
+
+if (delta < 0):
+    print("Equação sem raízes reais")
+elif (delta == 0):
+    resultado = (-b) / 2 * a
+    print(f"A raíz da equação é: {resultado}")
+else:
+    res1 = (-b + sqrt(delta)) / 2 * a
+    res2 = (-b - sqrt(delta)) / 2 * a
+    print(f"As raízes da equação são: {res1} e {res2}")

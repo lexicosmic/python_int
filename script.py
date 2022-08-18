7911901948a4c29c644f7ef0a96e3f01f4a25552
devgabmal <devgabmal@gmail.com>
2022-08-18 04:47:13 -0300

    Utiliza operadores relacionais

--- script.py
+++ script.py
@@ -10,15 +10,26 @@
 # print(10//3)
 # print(10 % 3)
 
-# Variáveis
-minha_variavel = "Olá, mundo!"
-print(minha_variavel)
-inteira = 1  # integer
-real = 1.1  # float
-textual = "Eu sou uma string"  # string
-booleana = True  # bool
-booleana = False  # bool
-print(inteira)
-print(real)
-print(textual)
-print(booleana)
+# # Variáveis
+# minha_variavel = "Olá, mundo!"
+# print(minha_variavel)
+# inteira = 1  # integer
+# real = 1.1  # float
+# textual = "Eu sou uma string"  # string
+# booleana = True  # bool
+# booleana = False  # bool
+# print(inteira)
+# print(real)
+# print(textual)
+# print(booleana)
+
+# Operadores relacionais
+var_a = 4
+var_b = 4
+print(var_a == var_b)
+var_a = 2
+var_b = 3
+print(var_a == var_b)
+print(var_a < var_b)
+soma = var_a+var_b
+print(soma < var_a)

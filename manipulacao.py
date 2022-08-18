4bbb5c74397d10a70840fcb75d49fdefdaed5a49
devgabmal <devgabmal@gmail.com>
2022-08-18 07:09:43 -0300

    Lê um arquivo

---
+++ manipulacao.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+arquivo = open("arquivo.txt")
+
+# linhas = arquivo.readlines()
+
+# for linha in linhas:
+#     print(linha)
+
+texto_completo = arquivo.read()
+print(texto_completo)

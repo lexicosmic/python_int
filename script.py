7a2b3fd02186aff19c6a052ba284203b70c9b15c
devgabmal <devgabmal@gmail.com>
2022-08-18 04:58:38 -0300

    Utiliza estruturas condicionais

--- script.py
+++ script.py
@@ -34,10 +34,29 @@
 # soma = var_x + var_y
 # print(soma < var_x)
 
-# Operadores lógicos
-var_x = 3
-var_y = 3
-var_z = 4
-print(var_x == var_y and var_x == var_z)
-print(var_x == var_y or var_x == var_z)
-print(var_x == var_y or var_x == var_z and var_z == var_y)
+# # Operadores lógicos
+# var_x = 3
+# var_y = 3
+# var_z = 4
+# print(var_x == var_y and var_x == var_z)
+# print(var_x == var_y or var_x == var_z)
+# print(var_x == var_y or var_x == var_z and var_z == var_y)
+
+# Estruturas condicionais
+var_x = 1
+var_y = 1000000
+if var_x > var_y:
+    print("X é maior que Y")
+elif var_y > var_x:
+    print("Y é maior que X")
+else:
+    print("X é igual a Y")
+
+var_x = 1
+var_y = 1
+if var_x > var_y:
+    print("X é maior que Y")
+elif var_y > var_x:
+    print("Y é maior que X")
+else:
+    print("X é igual a Y")
